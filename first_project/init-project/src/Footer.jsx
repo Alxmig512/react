4d5f90738bc12a033d596918beba1998804113d8
Alxmig512 <123176040+Alxmig512@users.noreply.github.com>
2025-03-03 20:46:36 +0100

    Initial commit

---
+++ first_project/init-project/src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+    return (
+            <footer>
+                <p>
+                    &copy; {new Date().getFullYear()} Alex ltd.
+                </p>
+            </footer>
+    );
+}
+
+export default Footer
