a9afc63895f8ff21f0922ec707998652a51df1a0
Alxmig512 <123176040+Alxmig512@users.noreply.github.com>
2025-03-14 19:38:16 +0100

    learned conditional rendering

---
+++ first_project/conditional rendering/src/App.jsx
@@ -0,0 +1,11 @@
+import UserGreeting from "./UserGreeting.jsx"
+function App() {
+
+    return(
+        <>
+            <UserGreeting isLoggedIn={false} username="Alex"/>
+        </>
+    )
+}
+
+export default App
